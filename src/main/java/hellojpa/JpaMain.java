a87812b121be64e9b1f16bb4b2b7ae9f360b7c88
bsh6463 <bsh9510@gmail.com>
2021-08-08 20:42:02 +0900

    210808 Hello JPA

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> memberList = em.createQuery("select  m from Member m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : memberList) {
+                System.out.println("member id = " + member.id);
+                System.out.println("member name = " + member.getName());
+            }
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
