25ba21c3208f87705e5f564a5ded56ad7b481f52
bsh6463 <bsh9510@gmail.com>
2021-08-12 21:34:07 +0900

    210812 CASCADE, orphanRemoval

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,22 +15,20 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1  = new Child();
+            Child child2  = new Child();
 
-            Member member = new Member();
-            member.setUserName("hello");
-            member.setTeam(team);
-            em.persist(member);
-            
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+
             em.flush();
             em.clear();
 
-         //   Member m = em.find(Member.class, member.getId());
-            List<Member> memberList = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
-
-
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
 
