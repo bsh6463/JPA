40aeee4f2d2a2d4385e47f9518650368eceea16b
bsh6463 <bsh9510@gmail.com>
2021-08-13 21:38:06 +0900

    210813 값 타입 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,6 +4,10 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
+        Address address1 = new Address("city", "street", "321321");
+        Address address2 = new Address("city", "street", "321321");
 
+        System.out.println("address1 == address2 " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
     }
 }
